e640039e26de2f8dc10ba151c32fcbcc10d6bd2a
Nikita Semenov <nikitasemenov@x5.ru>
2024-05-22 00:08:23 +0300

    hw02 some editions

--- hw02/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw02/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,23 +1,22 @@
 package ru.otus.hw.dao;
 
 import com.opencsv.bean.CsvToBean;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ru.otus.hw.config.TestFileNameProvider;
 import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 import ru.otus.hw.utils.CsvToBeanUtil;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Component
+@Repository
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
 
